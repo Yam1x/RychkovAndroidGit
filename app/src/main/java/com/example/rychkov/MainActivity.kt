391b82c2510d9d2f6feed435ef4afae1259fc0dc
Yam1x <rychkov03@yandex.ru>
2025-02-24 17:19:00 +0500

    Init

--- app/src/main/java/com/example/rychkov/MainActivity.kt
+++ app/src/main/java/com/example/rychkov/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RychkovTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Mr. Max",
+                        name = "Mr. Max2",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     RychkovTheme {
-        Greeting("Mr. Max")
+        Greeting("Mr. Max2")
     }
 }
